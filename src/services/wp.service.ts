f561934dfcf99afb648e79c82c697694b0f48544
Sky <wilson.skylerm@gmail.com>
2018-05-24 06:49:19 -0700

    0

--- src/services/wp.service.ts
+++ src/services/wp.service.ts
@@ -1,4 +1,3 @@
-configUrl = '../server-config.json';
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -7,33 +6,38 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class EmpService {
   employees=[];
-  constructor(private _http:Http) { }
+  constructor(private _http:HttpClient) { }
   checkMe:any;
-  getEmployees(){
-    return this._http.get("http://localhost/api/select.php/")
-      .map(res=>{
-        this.checkMe = res;
+  configUrl = '../server-config.json';
 
-        if(this.checkMe._body !== "0"){
-           return res.json()
-        }
 
-      });
-  }
-  addEmployee(info){
-    return this._http.post("http://localhost/api/insert.php",info)
-      .map(()=>"");
-  }
-  getEmployee(id){
-    return this._http.post("http://localhost/api/selectone.php/",{'id':id})
-      .map(res=>res.json());
-  }
-  deleteEmployee(id){
-    return this._http.post("http://localhost/api/delete.php/",{'id':id})
-      .map(()=>this.getEmployees());
-  }
-  updateEmployee(info){
-    return this._http.post("http://localhost/api/update.php/", info)
-      .map(()=>"");
-  }
+
+  // 
+  // getEmployees(){
+  //   return this._http.get("http://localhost/api/select.php/")
+  //     .map(res=>{
+  //       this.checkMe = res;
+  //
+  //       if(this.checkMe._body !== "0"){
+  //          return res.json()
+  //       }
+  //
+  //     });
+  // }
+  // addEmployee(info){
+  //   return this._http.post("http://localhost/api/insert.php",info)
+  //     .map(()=>"");
+  // }
+  // getEmployee(id){
+  //   return this._http.post("http://localhost/api/selectone.php/",{'id':id})
+  //     .map(res=>res.json());
+  // }
+  // deleteEmployee(id){
+  //   return this._http.post("http://localhost/api/delete.php/",{'id':id})
+  //     .map(()=>this.getEmployees());
+  // }
+  // updateEmployee(info){
+  //   return this._http.post("http://localhost/api/update.php/", info)
+  //     .map(()=>"");
+  // }
 }
