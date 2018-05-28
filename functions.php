541884717037e6db9310c6abe25f9f5df5d821f1
Sky <wilson.skylerm@gmail.com>
2018-05-28 00:23:15 -0700

    0

--- functions.php
+++ functions.php
@@ -1,2 +1,7 @@
 <?php
+// Enable the option show in rest
+add_filter( 'acf/rest_api/field_settings/show_in_rest', '__return_true' );
+
+// Enable the option edit in rest
+add_filter( 'acf/rest_api/field_settings/edit_in_rest', '__return_true' );
 ?>
