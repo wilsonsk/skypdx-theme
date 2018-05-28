212b7ee4779aa65f8e206ea815f391c658f0a495
Sky <wilson.skylerm@gmail.com>
2018-05-28 00:25:21 -0700

    0

--- functions.php
+++ functions.php
@@ -1,2 +1,8 @@
 <?php
+// Following 2 filters enable Advanced Custom Fields to be exposed via WP REST API
+// Enable the option show in rest
+add_filter( 'acf/rest_api/field_settings/show_in_rest', '__return_true' );
+
+// Enable the option edit in rest
+add_filter( 'acf/rest_api/field_settings/edit_in_rest', '__return_true' );
 ?>
