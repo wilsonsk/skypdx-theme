aace165d89e3ddcf65d7a2f9b3b9f659a3b9859f
Sky <wilson.skylerm@gmail.com>
2018-06-11 23:21:03 -0700

    0

--- src/app/models/complementary-colors.model.ts
+++ src/app/models/complementary-colors.model.ts
@@ -1,3 +1,3 @@
 export class ComplementaryColors {
-  constructor(public primaryColor:string, public secondaryColor:string, public tertiaryColor:string) {}
+  constructor(public primaryColor:string, public secondaryColor:string, public tertiaryColor:string, public textColorPrimary:string, public textColorSecondary:string) {}
 }
