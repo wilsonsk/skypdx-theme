e5634575e0fab83ab5b15070708c4022b67d3d9b
Sky <wilson.skylerm@gmail.com>
2018-07-11 22:33:25 -0700

    Established base header menu component

--- src/app/header-menu/header-menu.component.ts
+++ src/app/header-menu/header-menu.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { StateService } from '../services/state.service';
+import { State } from '../models/state.model';
 
 @Component({
   selector: 'app-header-menu',
   templateUrl: './header-menu.component.html',
   styleUrls: ['./header-menu.component.scss']
 })
-export class HeaderMenuComponent implements OnInit {
+export class HeaderMenuComponent implements OnInit, OnDestroy {
+  private stateChangedSubscription: Subscription;
 
-  constructor() { }
+  state: State;
+
+  constructor(private stateService: StateService) {
+  }
 
   ngOnInit() {
+    this.state = this.stateService.getState();
+    this.stateChangedSubscription = this.stateService.stateChanged.subscribe((stateCopy:State) => {
+      this.state = stateCopy;
+    });
+  }
+
+  ngOnDestroy() {
+    this.stateChangedSubscription.unsubscribe();
+  }
+
+  test() {
+    this.stateService.setState('gridIsOpen', true);
   }
 
 }
