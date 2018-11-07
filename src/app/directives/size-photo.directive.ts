b9d4562dc9aa6f72d2a8d72681b50bbde32722a7
Sky <wilson.skylerm@gmail.com>
2018-11-07 10:35:48 -0800

    Updated grid layout and sizes

--- src/app/directives/size-photo.directive.ts
+++ src/app/directives/size-photo.directive.ts
@@ -1,4 +1,8 @@
 import { OnInit, Directive, HostBinding } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { State } from '../models/state.model';
+import { StateService } from '../services/state.service';
 
 @Directive({
   selector: '[appSizePhoto]'
@@ -12,32 +16,44 @@ export class SizePhotoDirective implements OnInit {
   widthClasses = [
     'photo-grid__frame--width-1', 'photo-grid__frame--width-2', 'photo-grid__frame--width-3', 'photo-grid__frame--width-4'
   ];
+  state: State;
+  private stateChangedSubscription: Subscription;
 
-  constructor() { }
+  constructor(private stateService: StateService) { }
 
   ngOnInit() {
+    this.state = this.stateService.getState();
+    this.stateChangedSubscription = this.stateService.stateChanged.subscribe((stateCopy:State) => {
+      this.state = stateCopy;
+    });
     this.getWidthClass();
   }
 
   getWidthClass() {
-    const widthSelector = Math.floor(Math.random() * this.widthClasses.length);
+    if(this.state.browserIsLg) {
+      // const widthSelector = Math.floor(Math.random() * this.widthClasses.length);
+      //
+      // switch(this.widthClasses[widthSelector]) {
+      //   case 'photo-grid__frame--width-1':
+      //     this.w4 = true;
+      //     break;
+      //   case 'photo-grid__frame--width-2':
+      //     this.w3 = true;
+      //     break;
+      //   case 'photo-grid__frame--width-3':
+      //     this.w3 = true;
+      //     break;
+      //   case 'photo-grid__frame--width-4':
+      //     this.w4 = true;
+      //     break;
+      // }
+      this.w3 = true;
 
-    // switch(this.widthClasses[widthSelector]) {
-    //   case 'photo-grid__frame--width-1':
-    //     this.w1 = true;
-    //     break;
-    //   case 'photo-grid__frame--width-2':
-    //     this.w2 = true;
-    //     break;
-    //   case 'photo-grid__frame--width-3':
-    //     this.w3 = true;
-    //     break;
-    //   case 'photo-grid__frame--width-4':
-    //     this.w4 = true;
-    //     break;
-    // }
+    } else {
+      this.w3 = true;
+    }
 
-    this.w3 = true;
+
 
   }
 }
